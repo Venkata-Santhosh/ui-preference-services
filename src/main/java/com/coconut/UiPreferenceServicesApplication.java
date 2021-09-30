aed8f8189acbb84fe2abf7b0176bfdde2ac83f3f
Venkata-Santhosh <santhosh.piduri@gmail.com>
2021-09-30 19:04:45 -0400

    first commit

---
+++ src/main/java/com/coconut/UiPreferenceServicesApplication.java
@@ -0,0 +1,13 @@
+package com.coconut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiPreferenceServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiPreferenceServicesApplication.class, args);
+	}
+
+}
